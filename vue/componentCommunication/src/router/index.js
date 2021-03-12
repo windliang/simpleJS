6309882ae366b852c2a0f0ce1328f2f98eaf514b
windliang <1218041443@qq.com>
2021-03-12 11:24:54 +0800

    已删除        vue/componentCommunication/package-lock.json 已修改        vue/componentCommunication/package.json 已修改        vue/componentCommunication/src/router/index.js

--- vue/componentCommunication/src/router/index.js
+++ vue/componentCommunication/src/router/index.js
@@ -5,8 +5,8 @@ import porpParent from '@/components/porps&$emit/parent'
 import refParent from '@/components/ref&refs/parent'
 import eventBusParent from '@/components/eventBus/parent'
 import vuexParent from '@/components/vuex/parent'
-import attrsParent from '@/components/$attrs&$listeners/parent'
-import child2Parent from '@/components/$children&$parent/parent'
+// import attrsParent from '@/components/$attrs&$listeners/parent'
+// import child2Parent from '@/components/$children&$parent/parent'
 
 Vue.use(Router)
 
@@ -37,15 +37,15 @@ export default new Router({
       name: 'vuexParent',
       component: vuexParent
     },
-    {
-      path: '/attrsParent',
-      name: 'attrsParent',
-      component: attrsParent
-    },
-    {
-      path: '/child2Parent',
-      name: 'child2Parent',
-      component: child2Parent
-    }
+    // {
+    //   path: '/attrsParent',
+    //   name: 'attrsParent',
+    //   component: attrsParent
+    // },
+    // {
+    //   path: '/child2Parent',
+    //   name: 'child2Parent',
+    //   component: child2Parent
+    // }
   ]
 })
