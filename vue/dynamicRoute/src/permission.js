2b867dc1701e6ea69ac555682022e2af80b0ad25
windliang <1218041443@qq.com>
2021-04-15 21:04:17 +0800

    updata

--- vue/dynamicRoute/src/permission.js
+++ vue/dynamicRoute/src/permission.js
@@ -2,11 +2,11 @@ import router from './router'
 
 router.beforeEach((to, from, next) => {
     let permArr = ['navPage2A']
-    console.log('xiugvai',to, from)
+    console.log(to, from)
     if (permArr.indexOf(to.name) === -1) {
         next()
     } else {
-        // next({path: '/', replace: true}) // 跳转成功了，但会报错
+        // next({path: '/', replace: true}) // 跳转成功了，但抛出错误
         router.push({ path: '/' }).catch(err => {})
     }
 })
